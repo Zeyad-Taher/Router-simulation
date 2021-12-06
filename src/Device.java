7e4894e57d1bb23d0c531adacf9777ae75d347d6
Your Name <you@example.com>
2021-12-06 19:31:15 +0200

    added some log messages

---
+++ src/Device.java
@@ -0,0 +1,58 @@
+import static java.lang.Thread.sleep;
+
+public class Device implements Runnable {
+    private String name;
+    private String type;
+    private Router router;
+
+    public Device(String name,String type,Router router){
+        this.name=name;
+        this.type=type;
+        this.router=router;
+    }
+
+    @Override
+    public void run() {
+        connect();
+        try {
+            sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        performOnlineActivity();
+        try {
+            sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        disconnect();
+    }
+
+    public void connect()
+    {
+        router.occupy(this);
+        System.out.println("login");
+    }
+    public void performOnlineActivity()
+    {
+        System.out.println(this.name+" performs online activity");
+    }
+    public void disconnect()
+    {
+        router.release(this);
+        System.out.println("Logged out");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + type + ")";
+    }
+}
---
+++ src/Semaphore.java
@@ -0,0 +1,34 @@
+public class Semaphore {
+    protected int value = 0;
+    protected Semaphore()
+    {
+        value = 0;
+    }
+    protected Semaphore(int initial)
+    {
+        value = initial;
+    }
+    public synchronized void P(Device device) // wait
+    {
+        value--;
+        if (value < 0) {
+            try
+            {
+                System.out.println(device + " arrived and waiting");
+                wait();
+            }
+            catch (InterruptedException e) {}
+        }
+        else {
+            System.out.println(device + "arrived");
+        }
+    }
+    public synchronized void V(Device device) // Signal
+    {
+        System.out.println(device.getName() + ": Logged out");
+        value++ ;
+        if (value <= 0) {
+            notify();
+        }
+    }
+}
