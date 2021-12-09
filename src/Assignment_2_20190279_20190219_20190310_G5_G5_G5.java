fa071e23e2a228525a1d67fbcb07452eba70492c
Your Name <you@example.com>
2021-12-09 20:46:10 +0200

    added Network commented code

--- src/Assignment_2_20190279_20190219_20190310_G5_G5_G5.java
+++ src/Assignment_2_20190279_20190219_20190310_G5_G5_G5.java
@@ -314,3 +314,30 @@ class GUI implements ActionListener {
         GUI gui = new GUI();
     }
 }
+
+//class Network {
+//
+//    public static void main(String[] args) throws IOException {
+//        int N,TC;
+//        String TCLine;
+//        Scanner read = new Scanner(System.in);
+//        System.out.println("What is the number of WI-FI Connections?");
+//        N = read.nextInt();
+//        read.nextLine();
+//        Router router=new Router(N);
+//        ArrayList<Thread> devices = new ArrayList<>();
+//        System.out.println("What is the number of devices Clients want to connect?");
+//        TC = read.nextInt();
+//        read.nextLine();
+//        for (int i=0;i<TC;i++){
+//            TCLine = read.nextLine();
+//            String[] line = TCLine.split(" ");
+//            Thread device = new Thread(new Device(line[0],line[1],router));
+//            devices.add(device);
+//        }
+//
+//        for (Thread device : devices) {
+//            device.start();
+//        }
+//    }
+//}
